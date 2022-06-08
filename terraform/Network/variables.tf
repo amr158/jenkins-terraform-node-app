368a490fbffd4377ec966af65d4542ea2c51582c
amr ahmed <amr2017170269@cis.asu.edu.eg>
2022-06-08 19:01:40 +0200

    edit ec2

--- terraform/Network/variables.tf
+++ terraform/Network/variables.tf
@@ -10,17 +10,12 @@ variable "public_subnet_1_cidr" {
   type = string
 }
 
-variable "public_subnet_2_cidr" {
-  type = string
-}
+
 
 variable "private_Subnet_1_cidr" {
   type = string
 }
 
-variable "private_subnet_2_cidr" {
-  type = string
-}
 
 variable "region" {
   type = string
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,3 @@
 output "bastion1-ip" {
     value = aws_instance.bastion1.public_ip
 }
-output "bastion2-ip" {
-    value = aws_instance.bastion2.public_ip
-}
