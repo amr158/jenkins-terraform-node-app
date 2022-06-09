b8da5d346c721ee959498479bb2917696912da6f
amr ahmed <amr2017170269@cis.asu.edu.eg>
2022-06-09 04:22:05 +0200

    add jumphost

--- jumphost.sh
+++ jumphost.sh
@@ -1,6 +1,7 @@
 bastion_ip=`terraform output -json bastion1-ip | tr -d '"'`
 private_ip=`terraform output -json private-ip | tr -d '"'`
-
+cp terraform/key-pair.pem ~/.ssh/private_key.pem
+chmod 600 ~/.ssh/private_key.pem
 cat << EOF > ~/.ssh/config
 Host ${private_ip}
     ProxyCommand ssh -W %h:%p ${bastion_ip}
