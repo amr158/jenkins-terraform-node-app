03616f3f5776588c75c12c8f625bfeae4bf3b58b
amr ahmed <amr2017170269@cis.asu.edu.eg>
2022-06-09 04:02:17 +0200

    add private subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,6 @@ module "network" {
     vpc_cidr = var.vpc_cidr
     public_subnet_1_cidr = var.public_subnet_1_cidr
     private_Subnet_1_cidr = var.private_Subnet_1_cidr
-    private_Subnet_2_cidr = var.private_Subnet_2_cidr
+    private_subnet_2_cidr = var.private_subnet_2_cidr
     region = var.region
 }
