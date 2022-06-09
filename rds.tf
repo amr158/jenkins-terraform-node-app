b07053168a9adf2b5ead81ecfea75f56254131a2
amr ahmed <amr2017170269@cis.asu.edu.eg>
2022-06-09 02:40:58 +0200

    update Jenkinsfile

--- rds.tf
+++ rds.tf
@@ -13,6 +13,6 @@ resource "aws_db_instance" "rds" {
   password             = var.db_Pass  # ---> should be variable
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
-vpc_security_group_ids = [aws_security_group.DB-sgr.id]
+  db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.DB-sgr.id]
 }
