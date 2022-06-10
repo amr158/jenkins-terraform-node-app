b54b5e6b2bddb58159893e6b952fd8583d365463
amr ahmed <amr2017170269@cis.asu.edu.eg>
2022-06-10 13:47:43 +0200

    update Jenkinsfile

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -22,6 +22,11 @@ resource "aws_instance" "bastion1" {
   subnet_id = module.network.pub_sub1_id
   security_groups = [aws_security_group.security_group_1.id]
   key_name = aws_key_pair.instance.key_name
+  disable_api_termination = false
+  ebs_optimized = false
+  root_block_device {
+    volume_size = "10"
+  }
   tags = {
     "Name" = "${var.name}-bastion1"
   }
@@ -40,6 +45,11 @@ resource "aws_instance" "application1" {
   subnet_id = module.network.pri_sub1_id
   security_groups = [aws_security_group.security_group_2.id]
   key_name = aws_key_pair.instance.key_name
+  disable_api_termination = false
+  ebs_optimized = false
+  root_block_device {
+    volume_size = "10"
+  }
   tags = {
     "Name" = "${var.name}-application1"
   }
